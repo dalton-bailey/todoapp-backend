3c2a41d64026b06387f024530cdc181a828758ae
Bailey Dalton <baidalton@gmail.com>
2020-10-24 12:52:29 -0600

    init and setup

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var http = require('http')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-http.createServer((req, res) => {
-}).listen(port)
+app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log("Server running on port 3000");
+});
